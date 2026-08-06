3072fed3af168c58f2b5403700a224cbdc46d34e
quanngo018 <quannog018@gmail.com>
2026-08-06 16:56:27 +0700

    Test prune conv0 and conv1 of YOLO26 by using Group L2 Norm to choosing channel indexes to prune

---
+++ examples/yolo26/tests/test_prune_1_conv_group_l2.py
@@ -0,0 +1,87 @@
+import torch_pruning as tp
+from ultralytics import YOLO
+import torch
+
+yolo = YOLO("/home/edabk/Workspace/Quan/third_party/Torch-Pruning/examples/yolo26/yolo26n.pt")
+raw_model = yolo.model
+raw_model.eval() # Set the model to evaluation mode
+
+target_conv = raw_model.model[0].conv
+next_conv = raw_model.model[1].conv
+
+print("type(target_conv):", type(target_conv))
+print("target_conv:", target_conv)
+print("target_conv.weight.shape:", target_conv.weight.shape)
+print("target_conv.in_channels:", target_conv.in_channels)
+print("target_conv.out_channels:", target_conv.out_channels)
+
+print("type(next_conv):", type(next_conv))
+print("next_conv:", next_conv)
+print("next_conv.weight.shape:", next_conv.weight.shape)
+print("next_conv.in_channels:", next_conv.in_channels)
+print("next_conv.out_channels:", next_conv.out_channels)
+
+dummy_input = torch.randn(1, 3, 640, 640) # Create a dummy input tensor with shape (1, 3, 640, 640) representing a batch of 1 image with 3 color channels and size 640x640
+
+raw_model.requires_grad_(True) # Set requires_grad to True for all parameters in the model to enable gradient computation
+print("target required_grad: ", target_conv.weight.requires_grad) # Check if the target convolutional layer's weights require gradients
+
+DG = tp.DependencyGraph()
+DG.build_dependency(
+    raw_model, 
+    dummy_input
+)
+
+# 1. All channels are candidates
+candidate_indexes = list(range(target_conv.out_channels))
+
+# 2. Build candidate group
+candidate_group = DG.get_pruning_group(
+    target_conv,
+    tp.prune_conv_out_channels,
+    idxs=candidate_indexes
+)
+
+# 3. Calculate Group L2 importance
+importance = tp.importance.GroupMagnitudeImportance(p=2)
+
+scores = importance(candidate_group)
+
+print("Importance scores:", scores)
+print("Importance scores shape:", scores.shape)
+
+# 4. Select k channels with lowest scores
+k = 2
+
+score_indexes = torch.argsort(scores)[:k]
+
+# Convert score positions -> actual channel indexes
+pruning_indexes = [
+    candidate_indexes[i] for i in score_indexes.tolist()
+]
+
+print("Selected pruning indexes:", pruning_indexes)
+print("Selected scores:", scores[score_indexes])
+
+# 5. Create a NEW group containing only selected channels
+pruning_group = DG.get_pruning_group(
+    target_conv,
+    tp.prune_conv_out_channels,
+    idxs=pruning_indexes
+)
+
+print(pruning_group)
+print("Group valid:", DG.check_pruning_group(pruning_group))
+
+print("Before pruning:")
+print("target_conv.weight.shape:", target_conv.weight.shape)
+print("next_conv.weight.shape:", next_conv.weight.shape)
+
+pruning_group.prune()
+
+print("After pruning:")
+print("target_conv.weight.shape:", target_conv.weight.shape)
+print("next_conv.weight.shape:", next_conv.weight.shape)
+
+with torch.no_grad():
+    output = raw_model(dummy_input)
